4b20ca63661ad059bc432fcf1e5dafb000205aef
ruilovechu <ruilovechu@live.com>
2018-06-22 01:09:47 +0800

    init

---
+++ slnZCSharpLib2017/ZWebSocketServer/FrameType.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ZWebSocketServer
+{
+    public enum FrameType : byte
+    {
+        Continuation,
+        Text,
+        Binary,
+        Close = 8,
+        Ping = 9,
+        Pong = 10,
+    }
+}
+
---
+++ slnZCSharpLib2017/ZWebSocketServer/Helpers/MonoHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ZWebSocketServer.Helpers
+{
+  public static class MonoHelper
+  {
+    public static bool IsRunningOnMono ()
+    {
+      return Type.GetType ("Mono.Runtime") != null;
+    }
+  }
+}
+
