ad3b974c8cc9683452a49ab6503c091180cbc22e
zhangjr <ruilovechu@live.com>
2019-05-06 13:46:23 +0800

    添加将字符串转为32位小写MD5字符串的方法

---
+++ slnZCSharpLib2017/ZMD5/ZMD5Helper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZMD5
+{
+    public class ZMD5Helper
+    {
+        /// <summary>
+        /// 将指定字符串按指定编码进行 MD5 计算（32位小写）
+        /// </summary>
+        /// <param name="input">将进行 MD5 计算的字符串原文</param>
+        /// <param name="encoding">指定的编码</param>
+        /// <returns>计算后的32位小写MD5值</returns>
+        public static string ToMD5(string input, Encoding encoding)
+        {
+            MD5 md5Hasher = MD5.Create();
+            byte[] data = md5Hasher.ComputeHash(encoding.GetBytes(input));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+    }
+}
---
+++ slnZCSharpLib2017/ZMD5Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZMD5Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string srcStr = "1122";
+            string md5edStr = ZMD5.ZMD5Helper.ToMD5(srcStr, Encoding.UTF8);
+            Console.WriteLine(md5edStr);
+            Console.ReadKey();
+        }
+    }
+}
