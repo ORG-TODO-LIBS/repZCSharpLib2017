ffef3c99d5c75fff2f0feb10fe46b0b622983efd
zhangjr <ruilovechu@live.com>
2019-05-06 13:30:34 +0800

    添加get请求方法及zhttp测试项目

---
+++ slnZCSharpLib2017/ZHttp/ZHttpHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZHttp
+{
+    /// <summary>
+    /// http 帮助类
+    /// </summary>
+    public class ZHttpHelper
+    {
+        /// <summary>
+        /// 发送 get 请求
+        /// </summary>
+        /// <param name="url">请求地址</param>
+        /// <param name="statusCode">请求响应状态码</param>
+        /// <returns>请求返回的数据</returns>
+        public static string Get(string url, out string statusCode)
+        {
+            string result = string.Empty;
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = httpClient.GetAsync(url).Result;
+                statusCode = response.StatusCode.ToString();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    result = response.Content.ReadAsStringAsync().Result;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ slnZCSharpLib2017/ZHttpTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZHttpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string statusCode;
+            string retdata = ZHttp.ZHttpHelper.Get("https://bimcomposer.probim.cn/api/prj/GetAllModels?ProjectID=c14075a2-6c91-1fdb-3fbe-76bf898c24cf", out statusCode);
+            Console.WriteLine(statusCode);
+            Console.ReadKey();
+        }
+    }
+}
