815106a9994eef53cf5e01cb00ed34603d890ba4
zhangjr <ruilovechu@live.com>
2019-05-11 22:03:05 +0800

    add ZLog methods

---
+++ slnZCSharpLib2017/ZLog/ZLogHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZLog
+{
+    /// <summary>
+    /// 日志帮助类
+    /// </summary>
+    public class ZLogHelper
+    {
+        /// <summary>
+        /// 写入日志
+        /// </summary>
+        /// <param name="logpath">日志文件路径</param>
+        /// <param name="content">日志内容（建议不要包含回车）</param>
+        /// <param name="newline">是否另起一行</param>
+        public static void WriteLog(string logpath, string content, bool newline)
+        {
+            string logDirPath = Path.GetDirectoryName(logpath);
+            if(!Directory.Exists(logDirPath))
+            {
+                Directory.CreateDirectory(logDirPath);
+            }
+            using (StreamWriter sw = System.IO.File.AppendText(logpath))
+            {
+                if (newline)
+                {
+                    sw.Write("\r\n");
+                }
+                sw.Write(content);
+                sw.Close();
+            }
+        }
+    }
+}
---
+++ slnZCSharpLib2017/ZLogTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZLogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZLog.ZLogHelper.WriteLog("D:/Logtest/log.txt", "haha", false);
+            ZLog.ZLogHelper.WriteLog("D:/Logtest/log.txt", "haha1", true);
+            ZLog.ZLogHelper.WriteLog("D:/Logtest/log.txt", "haha2", true);
+            ZLog.ZLogHelper.WriteLog("D:/Logtest/log.txt", "haha3", true);
+        }
+    }
+}
